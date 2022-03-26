1c1cf590fb383d55937eec1d65d5574f3330f8c1
n3sh-source <gustavo.carakaki@gmail.com>
2022-03-25 22:06:26 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  list: string[];
+  task: string;
+
+  constructor() {
+    this.list = [];
+    this.task = '';
+  }
+
+  showTarefa() {
+    return this.list;
+  }
+
+  save(): void {
+    localStorage.setItem('tasks', JSON.stringify(this.list));
+  }
+
+  addTarefa() {
+    this.list.push(this.task);
+    this.task = '';
+    this.save();
+  }
+
+  remove(taksX: number): void {
+    this.list.splice(taksX, 1);
+    this.save();
+  }
 }
